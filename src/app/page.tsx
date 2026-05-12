01f1ae8f16f28e4b36e9f59d50686e8bc3e45b8e
Antonio <antojoseabad@gmail.com>
2026-05-12 09:57:54 +0200

    feat(home): add hero section with background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import Image from "next/image";
+import heroImage from "@/public/heroImage.avif";
 
 export default function Home() {
-	return <main></main>;
+	return (
+		<main className="min-h-screen relative flex items-center justify-center">
+			<Image
+				src={heroImage}
+				alt="hero image"
+				fill
+				priority
+				className="object-cover"
+			/>
+			<div className="absolute inset-0 bg-linear-to-b from-black/80 via-black/60 to-black/80"></div>
+			<div className="relative z-50 text-center">
+				<h1 className="uppercase text-5xl font-bold tracking-wider text-white">
+					Full Stack Developer
+				</h1>
+				<p className="text-lg font-md tracking-wide">
+					Ideating, developing, breaking, testing, and learning at every step.
+				</p>
+			</div>
+		</main>
+	);
 }

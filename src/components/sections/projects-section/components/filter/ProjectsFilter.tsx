4898d3ff5a2eced945abd7855a8f047a4a5a5a8d
Antonio <antojoseabad@gmail.com>
2026-05-13 00:31:11 +0200

    refactor(filter): modularize filter

--- src/components/sections/projects-section/components/filter/ProjectsFilter.tsx
+++ src/components/sections/projects-section/components/filter/ProjectsFilter.tsx
@@ -1,51 +1,23 @@
 "use client";
 
 import { useState } from "react";
+import FilterButton from "./FilterButton";
+import FilterPanel from "./FilterPanel";
 
-interface Props {
-	activeFilter: string;
-	onFilterChange: (category: string) => void;
-}
-
-export default function ProjectFilters({
-	activeFilter,
-	onFilterChange,
-}: Props) {
-	const categories = ["Frontend", "Backend", "Full Stack"];
-
+export default function ProjectFilters() {
 	const [isFilterOpen, setIsFilterOpen] = useState(false);
-
-	const handleFilter = (category: string) => {
-		onFilterChange(category);
-		setIsFilterOpen(false);
-	};
 
 	return (
 		<div className="flex mb-10 justify-around items-center">
 			<div className="flex relative">
-				<button
+				<FilterButton
+					isOpen={isFilterOpen}
 					onClick={() => setIsFilterOpen(!isFilterOpen)}
-					className="border p-2 px-5 rounded-lg cursor-pointer"
-				>
-					Filtrar:
-				</button>
-				{isFilterOpen && (
-					<ul className="absolute left-full ml-1 flex whitespace-nowrap p-2 px-5 gap-5 border rounded-lg bg-neutral-900">
-						{categories.map((category) => (
-							<li key={category}>
-								<button
-									onClick={() => handleFilter(category)}
-									className="group/filters group:hover/filters:opacity-20 opacity-80 cursor-pointer hover:text-white"
-								>
-									{category}
-								</button>
-							</li>
-						))}
-					</ul>
-				)}
+				/>
+				{isFilterOpen && <FilterPanel />}
 			</div>
 			<button
-				onClick={() => onFilterChange("Todos")}
+				onClick={() => {}}
 				className="text-lg font-bold tracking-wide cursor-pointer text-neutral-400"
 			>
 				<span>Mostrar todos</span>

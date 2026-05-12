d42d97b47299c965e67390fbdda3625e738b8f92
Antonio <antojoseabad@gmail.com>
2026-05-12 15:00:15 +0200

    feat(filters): add category filtering to ProjectsSection

--- src/components/sections/projects-section/ProjectsSection.tsx
+++ src/components/sections/projects-section/ProjectsSection.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import { projects } from "@/src/data/projects";
 import ProjectCard from "./components/ProjectCard";
+import ProjectFilters from "./components/ProjectsFilter";
+import { useState } from "react";
 
-export default function () {
+export default function ProjectsSection() {
+	const [activeFilter, setActiveFilter] = useState("Todos");
+
+	const filteredProjects =
+		activeFilter === "Todos"
+			? projects
+			: projects.filter((project) => project.category === activeFilter);
+
 	return (
 		<section id="projects" className="min-h-screen py-20">
-			<h2 className="uppercase text-3xl font-bold tracking-wider text-white mb-5 text-center">
+			<h2 className="uppercase text-3xl font-bold tracking-wider text-white mb-10 text-center">
 				Proyectos
 			</h2>
+			<div className="max-w-7xl mx-auto">
+				<ProjectFilters
+					activeFilter={activeFilter}
+					onFilterChange={setActiveFilter}
+				/>
+			</div>
 			<div className="max-w-7xl mx-auto grid grid-cols-3 gap-5 gap-y-7 group/cards">
-				{projects.map((project) => (
+				{filteredProjects.map((project) => (
 					<ProjectCard project={project} key={project.id} />
 				))}
 			</div>
---
+++ src/components/sections/projects-section/components/ProjectsFilter.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+
+interface Props {
+	activeFilter: string;
+	onFilterChange: (category: string) => void;
+}
+
+export default function ProjectFilters({
+	activeFilter,
+	onFilterChange,
+}: Props) {
+	const categories = ["Frontend", "Backend", "Full Stack"];
+
+	const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+	const handleFilter = (category: string) => {
+		onFilterChange(category);
+		setIsFilterOpen(false);
+	};
+
+	return (
+		<div className="flex mb-10 justify-around items-center">
+			<div className="flex relative">
+				<button
+					onClick={() => setIsFilterOpen(!isFilterOpen)}
+					className="border p-2 px-5 rounded-lg"
+				>
+					Filtrar:
+				</button>
+				{isFilterOpen && (
+					<ul className="absolute left-full ml-1 flex whitespace-nowrap p-2 px-5 gap-5 border rounded-lg bg-neutral-900">
+						{categories.map((category) => (
+							<li>
+								<button key={category} onClick={() => handleFilter(category)}>
+									{category}
+								</button>
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+			<button
+				onClick={() => onFilterChange("Todos")}
+				className="text-lg font-bold tracking-wide cursor-pointer text-neutral-400"
+			>
+				<span>Mostrar todos</span>
+			</button>
+		</div>
+	);
+}

14e00ff375307b086f6ab32ffab4f8e2b2f1cb51
Antonio <antojoseabad@gmail.com>
2026-05-13 00:03:30 +0200

    feat(filter): add toggle button to open filters panel

---
+++ src/components/sections/projects-section/components/filter/FilterButton.tsx
@@ -0,0 +1,31 @@
+interface Props {
+	isOpen: boolean;
+	onClick: () => void;
+}
+
+export default function FilterButton({ isOpen, onClick }: Props) {
+	return (
+		<div
+			className="flex border p-2 px-5 rounded-lg cursor-pointer items-center gap-3"
+			onClick={() => onClick()}
+		>
+			<button type="button">Filtrar:</button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				className={`${isOpen ? "rotate-180" : "rotate-0"}`}
+			>
+				<path
+					fill="none"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="m10 17l5-5l-5-5"
+				/>
+			</svg>
+		</div>
+	);
+}

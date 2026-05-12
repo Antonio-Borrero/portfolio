2b30c8e34b9a5de7aadd6c3fd33da2c8d45f3a4d
Antonio <antojoseabad@gmail.com>
2026-05-12 10:03:45 +0200

    refactor(home): move hero section to independent component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-import Image from "next/image";
-import heroImage from "@/public/heroImage.avif";
+import HeroSection from "../components/sections/hero-section/HeroSection";
 
 export default function Home() {
 	return (
-		<main className="min-h-screen relative flex items-center justify-center">
-			<Image
-				src={heroImage}
-				alt="hero image"
-				fill
-				priority
-				className="object-cover"
-			/>
-			<div className="absolute inset-0 bg-linear-to-b from-black/80 via-black/60 to-black/80"></div>
-			<div className="relative z-50 text-center">
-				<h1 className="uppercase text-5xl font-bold tracking-wider text-white">
-					Full Stack Developer
-				</h1>
-				<p className="text-lg font-md tracking-wide">
-					Ideating, developing, breaking, testing, and learning at every step.
-				</p>
-			</div>
+		<main>
+			<HeroSection />
 		</main>
 	);
 }

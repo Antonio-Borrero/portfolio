3377f47540eb22595d836e4d56de4dcc72e18c14
Antonio <antojoseabad@gmail.com>
2026-05-13 18:07:43 +0200

    style(button): add hover effects to button

--- src/components/sections/projects-section/components/filter/FilterButton.tsx
+++ src/components/sections/projects-section/components/filter/FilterButton.tsx
@@ -6,16 +6,18 @@ interface Props {
 export default function FilterButton({ isOpen, onClick }: Props) {
 	return (
 		<div
-			className="flex border p-2 px-5 rounded-lg cursor-pointer items-center gap-3 text-lg tracking-wide text-neutral-400"
+			className={`flex border p-2 px-5 rounded-lg cursor-pointer items-center gap-3 text-lg tracking-wide text-neutral-400 hover:border-neutral-200 hover:text-neutral-200 ${isOpen ? "border-neutral-200" : ""}`}
 			onClick={() => onClick()}
 		>
-			<button type="button">Filtrar:</button>
+			<button type="button" className={`${isOpen ? "text-neutral-200" : ""}`}>
+				Filtrar:
+			</button>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="24"
 				height="24"
 				viewBox="0 0 24 24"
-				className={`${isOpen ? "rotate-180" : "rotate-0"}`}
+				className={`${isOpen ? "rotate-180 text-neutral-200" : "rotate-0"}`}
 			>
 				<path
 					fill="none"

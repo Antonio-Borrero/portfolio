020c218a5d675f106f8b0ce921164d7e32a7ce74
Antonio <antojoseabad@gmail.com>
2026-05-15 14:21:10 +0200

    styles(button): update styles on button when selected or hover

--- src/components/sections/projects-section/components/filter/FilterButton.tsx
+++ src/components/sections/projects-section/components/filter/FilterButton.tsx
@@ -6,10 +6,10 @@ interface Props {
 export default function FilterButton({ isOpen, onClick }: Props) {
 	return (
 		<div
-			className={`flex border p-2 px-5 rounded-lg cursor-pointer items-center gap-3 text-lg tracking-wide text-neutral-400 hover:border-neutral-200 hover:text-neutral-200 ${isOpen ? "border-neutral-200" : ""}`}
+			className={`flex border p-2 px-5 rounded-lg cursor-pointer items-center gap-3 text-lg tracking-wide text-neutral-400 hover:border-neutral-300 hover:text-neutral-300 ${isOpen ? "border-neutral-300" : ""}`}
 			onClick={() => onClick()}
 		>
-			<button type="button" className={`${isOpen ? "text-neutral-200" : ""}`}>
+			<button type="button" className={`${isOpen ? "text-neutral-300" : ""}`}>
 				Filtrar:
 			</button>
 			<svg
@@ -17,7 +17,7 @@ export default function FilterButton({ isOpen, onClick }: Props) {
 				width="24"
 				height="24"
 				viewBox="0 0 24 24"
-				className={`${isOpen ? "rotate-180 text-neutral-200" : "rotate-0"}`}
+				className={`${isOpen ? "rotate-180 text-neutral-300" : "rotate-0"}`}
 			>
 				<path
 					fill="none"

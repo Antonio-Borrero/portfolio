9989e48256263c6d9eeb3f4d153f45c2adc20edf
Antonio <antojoseabad@gmail.com>
2026-05-18 01:58:49 +0200

    refactor(card): update badge styles and group names in card component

--- src/components/sections/projects-section/components/ProjectCard.tsx
+++ src/components/sections/projects-section/components/ProjectCard.tsx
@@ -3,25 +3,27 @@ import ProjectInterface from "@/src/interfaces/project.interface";
 import Image from "next/image";
 
 interface Props {
-	project: ProjectInterface;
+  project: ProjectInterface;
 }
 
 export default function ProjectCard({ project }: Props) {
-	return (
-		<div className="relative border-2 hover:scale-105 rounded-2xl aspect-video group/stack opacity-70 group-hover/cards:opacity-40 hover:opacity-100 pointer-events-auto">
-			<span className="absolute -top-3 left-1/2 -translate-x-1/2 whitespace-nowrap z-20 bg-neutral-800/80 backdrop-blur-md p-1 px-2 rounded-md border border-white/30 text-xs tracking-wide w-fit text-white">
-				{project.type} / {project.category}
-			</span>
-			<div className="absolute inset-0 rounded-2xl overflow-hidden">
-				<Image
-					src={project.mainImage}
-					alt={project.name}
-					fill
-					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-					className="object-cover"
-				/>
-			</div>
-			<TechStack stack={project.techStack} />
-		</div>
-	);
+  return (
+    <div
+      className={`group/card pointer-events-auto relative aspect-video cursor-pointer rounded-2xl border-2 border-neutral-800 opacity-80 transition-all duration-500 ease-out group-hover/projects:scale-98 group-hover/projects:opacity-40 group-hover/projects:blur-[1px] hover:scale-105 hover:border-neutral-600 hover:opacity-100 hover:blur-none`}
+    >
+      <span className="absolute top-0 left-1/2 z-20 -translate-x-1/2 rounded-md border-white/30 bg-neutral-800/60 p-1 px-2 text-xs tracking-wide whitespace-nowrap text-white opacity-0 backdrop-blur-md transition-all duration-500 group-hover/card:-translate-y-3 group-hover/card:opacity-100">
+        {project.type} / {project.category}
+      </span>
+      <div className="absolute inset-0 overflow-hidden rounded-2xl">
+        <Image
+          src={project.mainImage}
+          alt={project.name}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="object-cover"
+        />
+      </div>
+      <TechStack stack={project.techStack} />
+    </div>
+  );
 }

70626c3decd0c6ec0743241779714aad502f2259
Antonio <antojoseabad@gmail.com>
2026-05-18 01:55:16 +0200

    refactor(ux): implement cascade animation on stack badges

--- src/components/shared/tech-stack/TechStack.tsx
+++ src/components/shared/tech-stack/TechStack.tsx
@@ -1,18 +1,21 @@
 import { ICON_MAP } from "./icon-mapper";
 
 export default function TechStack({ stack }: { stack: string[] }) {
-	return (
-		<div className="absolute right-3 bottom-2 text-end">
-			<ul className=" flex flex-col items-end gap-1">
-				{stack.map((tech) => (
-					<li
-						key={tech}
-						className="bg-neutral-800/60 backdrop-blur-md p-2 rounded-full border border-white/30 text-xs w-fit group-hover/stack:opacity-100 opacity-0 transition-all duration-300 text-white"
-					>
-						{ICON_MAP[tech] || tech}
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="absolute right-3 bottom-2 text-end">
+      <ul className="flex flex-col items-end gap-1">
+        {stack.map((tech, index) => (
+          <li
+            key={tech}
+            style={{
+              transitionDelay: `${(stack.length - 1 - index) * 50}ms`,
+            }}
+            className="w-fit rounded-full border border-white/30 bg-neutral-800/60 p-2 text-xs text-white opacity-0 backdrop-blur-md transition-all duration-500 group-hover/card:-translate-y-2 group-hover/card:opacity-100"
+          >
+            {ICON_MAP[tech] || tech}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }

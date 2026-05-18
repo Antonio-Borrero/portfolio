6dc2ebad9249372abe4a60473f37c7b1a022a615
Antonio <antojoseabad@gmail.com>
2026-05-18 03:16:20 +0200

    feat(routing): setup parallel and intercepting routes for project modal details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,26 +4,29 @@ import "./globals.css";
 import Navbar from "../components/shared/Navbar";
 
 const montserrat = Montserrat({
-	variable: "--font-montserrat",
-	subsets: ["latin"],
+  variable: "--font-montserrat",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "Antonio Borrero",
-	description: "Personal web portfolio",
+  title: "Antonio Borrero",
+  description: "Personal web portfolio",
 };
 
 export default function RootLayout({
-	children,
+  children,
+  modal,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
+  modal?: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className={`${montserrat.variable} h-full antialiased`}>
-			<body className="min-h-full flex flex-col">
-				<Navbar />
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`${montserrat.variable} h-full antialiased`}>
+      <body className="flex min-h-full flex-col">
+        <Navbar />
+        {children}
+        {modal}
+      </body>
+    </html>
+  );
 }

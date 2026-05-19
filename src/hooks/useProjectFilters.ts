ef76766c37a6e67288fb6932e53810b79bbced9e
Antonio <antojoseabad@gmail.com>
2026-05-20 01:32:06 +0200

    fix(hooks):update comments to english

--- src/hooks/useProjectFilters.ts
+++ src/hooks/useProjectFilters.ts
@@ -3,77 +3,77 @@ import { FilterState } from "../interfaces/filters";
 import ProjectInterface from "../interfaces/project.interface";
 
 export default function useProjectFilters(projects: ProjectInterface[]) {
-	// -- ESTADO DE FILTROS --
+  // -- FILTERS STATE --
 
-	const [activeFilters, setActiveFilters] = useState<FilterState>({
-		category: "All",
-		type: "All",
-		techStack: [],
-	});
+  const [activeFilters, setActiveFilters] = useState<FilterState>({
+    category: "All",
+    type: "All",
+    techStack: [],
+  });
 
-	// -- HANDLERS: Cambian los filtros--
+  // -- HANDLERS: Change filters --
 
-	const handleCategoryClick = (category: string) => {
-		const newCategoryFilter =
-			activeFilters.category === category ? "All" : category;
-		setActiveFilters((prev) => ({
-			...prev,
-			category: newCategoryFilter,
-		}));
-	};
+  const handleCategoryClick = (category: string) => {
+    const newCategoryFilter =
+      activeFilters.category === category ? "All" : category;
+    setActiveFilters((prev) => ({
+      ...prev,
+      category: newCategoryFilter,
+    }));
+  };
 
-	const handleStackClick = (stack: string) => {
-		const currentStackFilter = activeFilters.techStack;
+  const handleStackClick = (stack: string) => {
+    const currentStackFilter = activeFilters.techStack;
 
-		const newStackFilter = currentStackFilter.includes(stack)
-			? currentStackFilter.filter((item) => item !== stack)
-			: [...currentStackFilter, stack];
-		setActiveFilters((prev) => ({
-			...prev,
-			techStack: newStackFilter,
-		}));
-	};
+    const newStackFilter = currentStackFilter.includes(stack)
+      ? currentStackFilter.filter((item) => item !== stack)
+      : [...currentStackFilter, stack];
+    setActiveFilters((prev) => ({
+      ...prev,
+      techStack: newStackFilter,
+    }));
+  };
 
-	const handleTypeClick = (type: string) => {
-		const newTypeFilter = activeFilters.type === type ? "All" : type;
-		setActiveFilters((prev) => ({
-			...prev,
-			type: newTypeFilter,
-		}));
-	};
+  const handleTypeClick = (type: string) => {
+    const newTypeFilter = activeFilters.type === type ? "All" : type;
+    setActiveFilters((prev) => ({
+      ...prev,
+      type: newTypeFilter,
+    }));
+  };
 
-	const handleReset = () => {
-		setActiveFilters({
-			category: "All",
-			type: "All",
-			techStack: [],
-		});
-	};
+  const handleReset = () => {
+    setActiveFilters({
+      category: "All",
+      type: "All",
+      techStack: [],
+    });
+  };
 
-	// -- FILTRO --
+  // -- FILTER --
 
-	const filteredProjects = useMemo(() => {
-		return projects.filter((project) => {
-			const matchCategory =
-				activeFilters.category === "All" ||
-				activeFilters.category === project.category;
-			const matchType =
-				activeFilters.type === "All" || activeFilters.type === project.type;
-			const mathTechStack =
-				activeFilters.techStack.length === 0 ||
-				activeFilters.techStack.every((stack) =>
-					project.techStack.includes(stack),
-				);
-			return matchCategory && matchType && mathTechStack;
-		});
-	}, [activeFilters, projects]);
+  const filteredProjects = useMemo(() => {
+    return projects.filter((project) => {
+      const matchCategory =
+        activeFilters.category === "All" ||
+        activeFilters.category === project.category;
+      const matchType =
+        activeFilters.type === "All" || activeFilters.type === project.type;
+      const mathTechStack =
+        activeFilters.techStack.length === 0 ||
+        activeFilters.techStack.every((stack) =>
+          project.techStack.includes(stack),
+        );
+      return matchCategory && matchType && mathTechStack;
+    });
+  }, [activeFilters, projects]);
 
-	return {
-		activeFilters,
-		handleCategoryClick,
-		handleTypeClick,
-		handleStackClick,
-		handleReset,
-		filteredProjects,
-	};
+  return {
+    activeFilters,
+    handleCategoryClick,
+    handleTypeClick,
+    handleStackClick,
+    handleReset,
+    filteredProjects,
+  };
 }

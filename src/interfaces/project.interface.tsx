0628bfea394839910403527113807a6089d94477
Antonio <antojoseabad@gmail.com>
2026-05-20 07:14:42 +0200

    feat(interfaces): add roadmap field to ProjectInterface

--- src/interfaces/project.interface.tsx
+++ src/interfaces/project.interface.tsx
@@ -2,28 +2,29 @@ import { StaticImageData } from "next/image";
 import { ReactNode } from "react";
 
 export const PROJECT_CATEGORIES = [
-	"Frontend",
-	"Backend",
-	"Full Stack",
+  "Frontend",
+  "Backend",
+  "Full Stack",
 ] as const;
 export const PROJECT_TYPES = [
-	"Proyecto personal",
-	"Proyecto de estudio",
+  "Proyecto personal",
+  "Proyecto de estudio",
 ] as const;
 
 export type ProjectCategory = (typeof PROJECT_CATEGORIES)[number];
 export type ProjectType = (typeof PROJECT_TYPES)[number];
 
 export default interface ProjectInterface {
-	id: string;
-	name: string;
-	shortDescription: string;
-	longDescription: string | ReactNode;
-	techStack: string[];
-	mainImage: string | StaticImageData;
-	gallery: (string | StaticImageData)[];
-	projectUrl: string | null;
-	githubUrl: string;
-	category: ProjectCategory;
-	type: ProjectType;
+  id: string;
+  name: string;
+  shortDescription: string;
+  longDescription: string | ReactNode;
+  techStack: string[];
+  mainImage: string | StaticImageData;
+  gallery: (string | StaticImageData)[];
+  projectUrl: string | null;
+  githubUrl: string;
+  category: ProjectCategory;
+  type: ProjectType;
+  roadmap?: string[];
 }

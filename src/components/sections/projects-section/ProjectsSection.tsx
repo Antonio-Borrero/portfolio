ac051651728bffbd4832d40ccc3e4893f8e55a25
Antonio <antojoseabad@gmail.com>
2026-05-20 22:46:50 +0200

    fix(projects: add missing key in projects list rendering

--- src/components/sections/projects-section/ProjectsSection.tsx
+++ src/components/sections/projects-section/ProjectsSection.tsx
@@ -42,7 +42,7 @@ export default function ProjectsSection() {
 
       <ul className="group/projects pointer-events-none mx-auto grid max-w-7xl grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {filteredProjects.map((project) => (
-          <li>
+          <li key={project.id}>
             <ProjectCard project={project} key={project.id} />
           </li>
         ))}

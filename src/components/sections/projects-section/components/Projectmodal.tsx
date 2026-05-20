ddd0b512b6211a7976f9a2f4c3c901214c4f5136
Antonio <antojoseabad@gmail.com>
2026-05-20 03:57:35 +0200

    feat(modal): add navigation between projects in modal

--- src/components/sections/projects-section/components/Projectmodal.tsx
+++ src/components/sections/projects-section/components/Projectmodal.tsx
@@ -1,11 +1,19 @@
 import Carrousel from "@/src/components/shared/Carrousel";
 import { ICON_MAP } from "@/src/constants/icon-mapper";
 import ProjectInterface from "@/src/interfaces/project.interface";
+import Link from "next/link";
+import { projects } from "@/src/data/projects";
 
 interface Props {
   project: ProjectInterface;
 }
 export default function ProjectModal({ project }: Props) {
+  const currentIndex = projects.findIndex((p) => p.id === project.id);
+
+  const prevProject =
+    projects[currentIndex - 1] || projects[projects.length - 1];
+  const nextProject = projects[currentIndex + 1] || projects[0];
+
   return (
     <div className="relative flex h-full min-h-0 w-full flex-col gap-6 text-neutral-200">
       {/* Header */}
@@ -18,8 +26,49 @@ export default function ProjectModal({ project }: Props) {
         </h1>
       </div>
 
-      {/* Carousel */}
-      <Carrousel gallery={project.gallery} />
+      <div className="relative">
+        {/* Navigation */}
+        <Link
+          href={prevProject.id}
+          className="absolute top-1/2 -left-10 z-20 -translate-y-1/2 cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1}
+            stroke="currentColor"
+            className="h-10 w-10 transition-transform duration-200 hover:scale-110"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </Link>
+        <Link
+          href={nextProject.id}
+          className="absolute top-1/2 -right-10 -translate-y-1/2 cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1}
+            stroke="currentColor"
+            className="h-10 w-10 transition-transform duration-200 hover:scale-110"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </Link>
+        {/* Carousel */}
+        <Carrousel gallery={project.gallery} />
+      </div>
 
       {/* Details: */}
       <div className="flex min-h-0 grow gap-8 overflow-hidden border-t border-neutral-700/80 pt-5">

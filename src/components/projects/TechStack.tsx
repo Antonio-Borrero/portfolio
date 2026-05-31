98082636a0c31332a54ef3855030f305cbd30484
Antonio <antojoseabad@gmail.com>
2026-06-01 00:23:05 +0200

    refactor(tech-stack): simplify component architecture and unify tooltip logic

--- src/components/projects/TechStack.tsx
+++ src/components/projects/TechStack.tsx
@@ -13,34 +13,26 @@ export default function TechStack({
   variant = "horizontal",
 }: Props) {
   return (
-    <>
-      {variant === "horizontal" ? (
-        <ul className={clsx("flex items-center gap-2", className)}>
-          {techStack.map((tech) => (
-            <li
-              key={tech}
-              className="flex w-fit items-center gap-2 rounded-full border border-white/30 bg-neutral-800/60 p-2 text-xs text-white opacity-80 backdrop-blur-md"
-            >
-              {ICON_MAP[tech]}
-              {tech}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <ul className={clsx("flex flex-col items-center gap-2", className)}>
-          {techStack.map((tech) => (
-            <li
-              key={tech}
-              className="group relative w-fit rounded-full border border-white/30 bg-neutral-800/60 p-2 text-xs text-white opacity-80 backdrop-blur-md hover:scale-120"
-            >
-              {ICON_MAP[tech] || tech}
-              <span className="pointer-events-none absolute top-1/2 left-full translate-x-2 -translate-y-1/2 rounded-xl border border-white/50 bg-neutral-800/80 p-2 whitespace-nowrap opacity-0 backdrop-blur-md transition-all duration-300 group-hover:opacity-100">
-                {tech}
-              </span>
-            </li>
-          ))}
-        </ul>
+    <ul
+      className={clsx(
+        "flex items-center gap-2",
+        className,
+        variant === "vertical" ? "flex-col" : "",
       )}
-    </>
+    >
+      {techStack.map((tech) => (
+        <li
+          key={tech}
+          className="group relative w-fit rounded-full border border-white/30 bg-neutral-800/60 p-2 text-xs text-white opacity-80 backdrop-blur-md hover:scale-120"
+        >
+          {ICON_MAP[tech] || tech}
+          <span
+            className={`pointer-events-none absolute rounded-2xl border border-white/50 bg-neutral-800/80 p-2 whitespace-nowrap opacity-0 backdrop-blur-md transition-all duration-300 group-hover:scale-100 group-hover:opacity-100 ${variant === "vertical" ? "top-1/2 right-full -translate-x-2 -translate-y-1/2" : "top-full left-1/2 -translate-x-1/2 translate-y-2"}`}
+          >
+            {tech}
+          </span>
+        </li>
+      ))}
+    </ul>
   );
 }

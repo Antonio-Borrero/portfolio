484e63c4bc6285f52f898373a33df108a551d68a
Antonio <antojoseabad@gmail.com>
2026-06-01 16:19:21 +0200

    refactor(projects): add carousel padding and move min-width to cards

--- src/components/projects/ProjectNavigator.tsx
+++ src/components/projects/ProjectNavigator.tsx
@@ -14,7 +14,10 @@ export default function ProjectNavigator({ currentProject }: Props) {
     <section className="custom-scrollbar w-full snap-x snap-mandatory overflow-x-auto scroll-smooth pt-5 pb-2">
       <ul className="group/projects flex flex-nowrap gap-3">
         {projectList.map((project) => (
-          <li key={project.id} className="min-w-48 shrink-0 snap-start">
+          <li
+            key={project.id}
+            className="shrink-0 snap-start first:pl-5 last:pr-5"
+          >
             <ProjectNavigatorCard project={project} />
           </li>
         ))}
